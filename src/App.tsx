98604d9f39a7a98f5281315195a5aa1eab4cbaca
Katorz <katorz.art@gmail.com>
2023-02-26 21:37:30 +0100

    Add states to Settings component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
+import { useState } from "react";
 import { Settings } from "./components/Settings";
 
 function App() {
+    const [width, setWidth] = useState(5);
+    const [height, setHeight] = useState(5);
+    const [rulesNum, setRulesNum] = useState(110);
     return (
         <>
-            <Settings />
+            <Settings
+                width={width}
+                height={height}
+                rules={rulesNum}
+                updateWidth={setWidth}
+                updateHeight={setHeight}
+                updateRules={setRulesNum}
+                onNextGeneration={() => {}}
+                onToggleAutoGeneration={() => {}}
+            />
         </>
     );
 }
--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,4 +1,38 @@
-export function Settings() {
+import { ChangeEvent } from "react";
+
+export function Settings({
+    width,
+    height,
+    rules,
+    updateWidth,
+    updateHeight,
+    updateRules,
+    onNextGeneration,
+    onToggleAutoGeneration,
+}: {
+    width: number;
+    height: number;
+    rules: number;
+    updateWidth: (x: number) => void;
+    updateHeight: (x: number) => void;
+    updateRules: (x: number) => void;
+    onNextGeneration: () => void;
+    onToggleAutoGeneration: () => void;
+}) {
+    function getAsNumber(e: ChangeEvent<HTMLInputElement>) {
+        return e.target.valueAsNumber;
+    }
+
+    function handleChange(
+        update: (x: number) => void
+    ): (e: ChangeEvent<HTMLInputElement>) => void {
+        return (e) => update(getAsNumber(e));
+    }
+
+    function setRandomRulesNum() {
+        return updateRules(Math.floor(Math.random() * 256));
+    }
+
     return (
         <div>
             <div>
@@ -6,11 +40,21 @@ export function Settings() {
                 <div>
                     <div>
                         <label>Width</label>
-                        <input type="number" min="0" />
+                        <input
+                            type="number"
+                            min="0"
+                            value={width}
+                            onChange={handleChange(updateWidth)}
+                        />
                     </div>
                     <div>
                         <label>Height</label>
-                        <input type="number" min="0" />
+                        <input
+                            type="number"
+                            min="0"
+                            value={height}
+                            onChange={handleChange(updateHeight)}
+                        />
                     </div>
                 </div>
             </div>
@@ -18,13 +62,25 @@ export function Settings() {
                 <h2>Generation</h2>
                 <label>Rules</label>
                 <div>
-                    <input type="number" min="0" max="255" />
-                    <button type="button">Random</button>
+                    <input
+                        type="number"
+                        min="0"
+                        max="255"
+                        value={rules}
+                        onChange={handleChange(updateRules)}
+                    />
+                    <button type="button" onClick={setRandomRulesNum}>
+                        Random
+                    </button>
                 </div>
             </div>
             <div>
-                <button type="button">Next generation</button>
-                <button type="button">Start auto evolution</button>
+                <button type="button" onClick={onNextGeneration}>
+                    Next generation
+                </button>
+                <button type="button" onClick={onToggleAutoGeneration}>
+                    Toggle auto evolution
+                </button>
             </div>
         </div>
     );
