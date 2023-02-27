eeaade2053a36feadf31d2560ca6b7e2844e61a2
Katorz <katorz.art@gmail.com>
2023-02-28 00:43:10 +0100

    Create Accessor and Initializer enums

---
+++ src/enums/Initializer.enum.ts
@@ -0,0 +1,8 @@
+type InitializerFunction = (x?: unknown, i?: number) => boolean;
+
+export const Initializer: {
+    [key: string]: InitializerFunction;
+} = Object.freeze({
+    RANDOM: () => Math.random() < 0.5,
+    FIRST: (_, i) => i === 0,
+});
